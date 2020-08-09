943f4276c704f14e537bb7013d5d87f9fe5a20e0
Wingmore <36834195+Wingmore@users.noreply.github.com>
2020-08-09 18:07:30 +1000

    Update LogReg.py
    Bug Fixes and new gradient descent method

--- ml_algorithms/LogReg.py
+++ ml_algorithms/LogReg.py
@@ -42,10 +42,10 @@ y = y.apply(y_classify)
 # Split data into train, test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=0)
 
-logReg = LogisticRegression()
-logReg = logReg.fit(X_train, y_train)
-y_pred = logReg.predict(X_test)
-print(r"Logistic Regression score for is {}".format(accuracy_score(y_pred, y_test)))
+# logReg = LogisticRegression()
+# logReg = logReg.fit(X_train, y_train)
+# y_pred = logReg.predict(X_test)
+# print(r"Logistic Regression score for is {}".format(accuracy_score(y_pred, y_test)))
 
 
 #%%
@@ -61,16 +61,16 @@ def sigmoid(z):
 def costFunction(theta, X, y, LRlambda):
     m = np.shape(y)[0]
     J = 0;
-    grad = np.zeros(np.shape(theta))
-    jj = -np.multiply(y,np.log(sigmoid(X*theta))) - np.multiply((1-y),np.log(1-sigmoid(X*theta)))                                                            
-    J = 1/m*sum[jj] + LRlambda/(2*m)*sum(theta[2:-1]**2) #DONT regularize theta0 since its bias term
+    jj = -np.multiply(y,np.log(sigmoid(np.dot(X,theta)))) - np.multiply((1-y),np.log(1-sigmoid(np.dot(X,theta))))                                                            
+    J = 1/m*np.sum(jj) + LRlambda/(2*m)*np.sum(theta[1:]**2) #DONT regularize theta0 since its bias term
     return J
 
 def gradFunction(theta, X, y, LRlambda):
     m = np.shape(y)[0]
-    grad_tmp = 1/m * X.T *(sigmoid(X*theta)-y)
-    grad = np.array([grad_tmp[1], grad_tmp[2:-1] + LRlambda/m*theta[2:-1]])
-    return grad
+    grad = np.zeros(np.shape(theta))
+    grad_tmp = 1/m * np.dot( X.T, (sigmoid(np.dot(X,theta))-y))
+    grad = np.insert(grad_tmp[1:] + LRlambda/m*theta[1:], 0, grad_tmp[0], axis=0)
+    return grad.flatten()
         
     
 def oneVsA(X, y, num_labels, LRlambda):
@@ -82,12 +82,14 @@ def oneVsA(X, y, num_labels, LRlambda):
     for c in np.arange(num_labels):
         initial_theta = np.zeros([n+1,1])
         args = ( X, (y == c), LRlambda)
-        result = optimize.fmin_cg(costFunction, initial_theta, fprime=gradFunction, args = args)
+        opts = {'maxiter': None, 'disp': True}
+        # result = optimize.fmin_cg(costFunction, initial_theta, fprime=gradFunction, args = args)
+        result = optimize.minimize(costFunction, initial_theta, jac=gradFunction, args=args,  method = 'Nelder-Mead', options=opts) 
         all_theta[c,:] = result
     return all_theta
 
 def findmax(l):
-    max_val = max(l)
+    max_val = np.max(l)
     max_idx = l.index(max_val)
     return max_idx, max_val
 
@@ -98,9 +100,20 @@ def predictOneVsA(all_theta, X):
     #add ones to beginning
     X = np.concatenate((np.ones([m,1]), X.to_numpy()), axis=1)
     A = np.dot(X,all_theta.T);
-    [I,V] = findmax(A);
+    I =  np.argmax(A, axis=1)
     return I
 
 regularization = 0.1;   #lambda
 num_labels = 3; #or 5
-[all_theta] = oneVsA(X, y, num_labels, regularization);
+all_theta = oneVsA(X, y, num_labels, regularization);
+y_pred = predictOneVsA(all_theta, X_test);
+accuracy = np.mean((y_pred == y_test))
+
+theta_t = np.array([-2, -1, 1, 2]).T
+X_t = np.concatenate((np.ones([5,1]), np.reshape(np.arange(1,16),[3,5]).T/10), axis=1)
+y_t = np.array([1,0,1,0,1]).T #([1;0;1;0;1] >= 0.5);
+lambda_t = 3;
+J = costFunction(theta_t, X_t, y_t, lambda_t)
+grad = gradFunction(theta_t, X_t, y_t, lambda_t)
+print(r'\nCost: {}\n'.format( J));
+print(r'Grad: {}'.format(grad))
